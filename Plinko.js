d3bf5d5ca9ad78bd58180b8f852b1f5fa7e761fe
Hriman-2412 <72973449+Hriman-2412@users.noreply.github.com>
2020-12-21 15:14:28 +0530

    Add files via upload

---
+++ Plinko.js
@@ -0,0 +1,28 @@
+class Plinko{
+
+constructor(x,y){
+
+var options={
+isStatic : true
+}
+
+
+this.body = Bodies.circle(x,y,10,options);
+World.add(world,this.body);
+
+}
+
+display(){
+
+var pos = this.body.position;
+
+push();
+translate(pos.x,pos.y);
+fill("white");
+ellipseMode(CENTER);
+ellipse(0,0,10,10);
+pop();
+
+}
+
+}
---
+++ sketch.js
@@ -0,0 +1,71 @@
+const Engine = Engine;
+const World = World;
+const Bodies = Bodies;
+
+var engine,world;
+
+var ground;
+
+var plinkos = [];
+var particles = [];
+var divisions = [];
+
+var divisionHeight = 300;
+
+function setup() {
+  createCanvas(480,800);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(240,height,480,20);
+
+ for (var k = 0; k <=width; k = k+80){
+
+divisions.push(new Divisions(k, height-divisionHeight/2,10,divisionHeight));
+
+ }
+
+ for (var j = 40; j <= width; j=j+50){
+
+ plinkos.push(new Plinko(j,75));
+
+ }
+
+ for (var j = 15; j <= width-10; j=j+50){
+
+  plinkos.push(new Plinko(j,175));
+ 
+  }
+ 
+  if(frameCount%60===0){
+
+particles.push(new Particle(random(width/2-10,width/2+10),10,10));
+
+  }
+
+}
+
+function draw(){
+background(0);
+
+ground.display();
+
+for(var j = 0; j<particles.length; j++ ){
+
+particles[j].display();
+}
+
+for(var i = 0; i<plinkos.length; j++ ){
+
+  plinkos[i].display();
+  }
+
+  for(var k = 0; k<divisions.length; k++ ){
+
+    divisions[k].display();
+    }
+    
+drawSprites();
+
+}
